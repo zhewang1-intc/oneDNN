21866f38a6f1b5b9addcb3fab94c8b72622454e6
ZheWang <zhe1.wang@intel.com>
2023-06-15 09:57:57 +0000

    vscode env settings, avoid preset when cmake-tool-kits enable.

--- get_dpcpp_compiler.sh
+++
@@ -1,3 +0,0 @@
-wget https://registrationcenter-download.intel.com/akdlm/IRC_NAS/89283df8-c667-47b0-b7e1-c4573e37bd3e/l_dpcpp-cpp-compiler_p_2023.1.0.46347_offline.sh
-chmod 777 l_dpcpp-cpp-compiler_p_2023.1.0.46347_offline.sh
-mv l_dpcpp-cpp-compiler_p_2023.1.0.46347_offline.sh ../ 
